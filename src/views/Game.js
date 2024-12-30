1fde7cda928e35b491be9952813a04bc78c3dfa8
Tobias Wiensgoll <tobias.wiensgoll@gmail.com>
2024-12-30 17:08:00 +0100

    neue parcel neue imports

--- src/views/Game.js
+++ src/views/Game.js
@@ -1,27 +1,50 @@
 import GameController from "../controller/GameController.js";
-import pngImages from "../assets/images/*.png";
-import jpgImages from "../assets/images/*.jpg";
-import SnakePngImages from "../assets/snake/*.png";
+
+// Manuelle Imports für die Bilder
+import appleImage from "../assets/images/apple.png";
+import foodImage from "../assets/images/food.png";
+import dungeonBackgroundImage from "../assets/images/dungeon_background.jpg";
+
+// Snake-Bilder importieren
+import headRight from "../assets/snake/head_right.png";
+import headLeft from "../assets/snake/head_left.png";
+import headUp from "../assets/snake/head_up.png";
+import headDown from "../assets/snake/head_down.png";
+import bodyHorizontal from "../assets/snake/body_horizontal.png";
+import bodyVertical from "../assets/snake/body_vertical.png";
+import bodyRightUp from "../assets/snake/body_rightup.png";
+import bodyRightDown from "../assets/snake/body_rightdown.png";
+import bodyDownRight from "../assets/snake/body_downright.png";
+import bodyUpRight from "../assets/snake/body_upright.png";
+import tailRight from "../assets/snake/tail_right.png";
+import tailLeft from "../assets/snake/tail_left.png";
+import tailUp from "../assets/snake/tail_up.png";
+import tailDown from "../assets/snake/tail_down.png";
+
 import Phaser from "phaser";
 
 export default class Game extends Phaser.Scene {
   preload() {
-    this.load.image("apple", pngImages.food);
-    this.load.image("snakeRight", SnakePngImages.head_right);
-    this.load.image("snakeLeft", SnakePngImages.head_left);
-    this.load.image("snakeUp", SnakePngImages.head_up);
-    this.load.image("snakeDown", SnakePngImages.head_down);
-    this.load.image("bodyHorizontal", SnakePngImages.body_horizontal);
-    this.load.image("bodyVertical", SnakePngImages.body_vertical);
-    this.load.image("bodyRightUp", SnakePngImages.body_rightup);
-    this.load.image("bodyRightDown", SnakePngImages.body_rightdown);
-    this.load.image("bodyDownRight", SnakePngImages.body_downright);
-    this.load.image("bodyUpRight", SnakePngImages.body_upright);
-    this.load.image("tailRight", SnakePngImages.tail_right);
-    this.load.image("tailLeft", SnakePngImages.tail_left);
-    this.load.image("tailUp", SnakePngImages.tail_up);
-    this.load.image("tailDown", SnakePngImages.tail_down);
-    this.load.image("dungeon_background", jpgImages.dungeon_background);
+    // Lade alle Bilder
+    this.load.image("apple", appleImage);
+    this.load.image("food", foodImage);
+    this.load.image("dungeon_background", dungeonBackgroundImage);
+
+    // Snake-Bilder laden
+    this.load.image("snakeRight", headRight);
+    this.load.image("snakeLeft", headLeft);
+    this.load.image("snakeUp", headUp);
+    this.load.image("snakeDown", headDown);
+    this.load.image("bodyHorizontal", bodyHorizontal);
+    this.load.image("bodyVertical", bodyVertical);
+    this.load.image("bodyRightUp", bodyRightUp);
+    this.load.image("bodyRightDown", bodyRightDown);
+    this.load.image("bodyDownRight", bodyDownRight);
+    this.load.image("bodyUpRight", bodyUpRight);
+    this.load.image("tailRight", tailRight);
+    this.load.image("tailLeft", tailLeft);
+    this.load.image("tailUp", tailUp);
+    this.load.image("tailDown", tailDown);
   }
 
   create() {
